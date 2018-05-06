27bc2de4133f4f037e5457cfa3c0e662f6cc27d2
arestrepoc <35736552+arestrepoc@users.noreply.github.com>
2018-05-06 16:00:20 -0500

    Add files via upload

---
+++ laboratorios/lab05/codigo/Pareja.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab5;
+
+import Talleres.*;
+
+public class Pareja
+{
+    public int vertice;
+    public int peso;
+    public Pareja(int v,int p) {vertice = v; peso = p;}
+}
